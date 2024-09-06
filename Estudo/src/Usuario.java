b29b98a2783cd8f959e544c7915e7535a4ff7a0a
Khristian-Tomaz <khristiantomaz@icloud.com>
2024-09-05 22:14:51 -0300

    a

---
+++ Estudo/src/Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+
+    public static void main(String[] args) {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Tv ligada:" + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Tv ligada :" + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Tv ligada :" + smartTv.ligada);
+    }
+}
